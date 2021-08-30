b0acb2ad6d7712e8f18cb0965a6340bcecc56725
alexi <alexi@DESKTOP-81UNKJJ>
2021-08-29 23:44:49 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1, numero2;
+
+            Console.Write("numero 1: ");
+            numero1 = int.Parse(Console.ReadLine());
+
+            Console.Write("numero 2: ");
+            numero2 = int.Parse(Console.ReadLine());
+
+            Console.Write("resultado " + (numero1 + numero2));
+
+            Console.ReadKey();
+        }
+    }
+}
